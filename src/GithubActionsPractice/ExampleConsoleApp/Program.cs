62d31c29500b8b9bcd8bfafdae70b7e06707a1a5
gnalvesteffer <gavin.alvesteffer@aristocrat.com>
2023-10-16 10:30:45 -0500

    refactor

--- src/GithubActionsPractice/ExampleConsoleApp/Program.cs
+++ src/GithubActionsPractice/ExampleConsoleApp/Program.cs
@@ -1,4 +1,19 @@
-﻿new ExampleClassLibrary.HelloWorldPrinter().Print();
-new ExampleClassLibrary.GuidPrinter().Print();
-new ExampleClassLibrary.TrophyPrinter().Print();
-new ExampleClassLibrary.BurgerPrinter().Print();
+﻿using ExampleClassLibrary;
+using System.Reflection;
+
+var exampleClassLibraryAssembly = Assembly.Load(nameof(ExampleClassLibrary));
+Console.WriteLine($"{nameof(ExampleClassLibrary)} v{exampleClassLibraryAssembly.GetName().Version}");
+
+var printerTypes = exampleClassLibraryAssembly.GetTypes().Where(type => typeof(IPrinter).IsAssignableFrom(type) && !type.IsInterface);
+foreach (Type printerType in printerTypes)
+{
+    if (Activator.CreateInstance(printerType) is IPrinter printer)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine($"{printerType}:");
+        Console.BackgroundColor = ConsoleColor.Black;
+        printer.Print();
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
